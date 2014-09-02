9d8a063eaf3e7e277326435531a6781a3c85e4af
George Miroshnykov <george.miroshnykov@gmail.com>
2014-09-02 16:37:36 +0300

    API: support for transplanting and squashing revsets

--- transplant.py
+++ transplant.py
@@ -9,9 +9,12 @@ from repository import Repository, MercurialException
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TRANSPLANT_FILTER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'transplant_filter.py')
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+TRANSPLANT_FILTER = os.path.join(PROJECT_DIR, 'transplant_filter.py')
 PULL_INTERVAL = 60
-MAX_COMMITS = 3
+MAX_COMMITS = 100
+
+Repository.register_extension('collapse', os.path.join(PROJECT_DIR, 'vendor', 'hgext', 'collapse.py'))
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -71,7 +74,7 @@ def clone_or_pull(name, refresh=False):
 
 def get_last_pull_date(name):
     repo_dir = get_repo_dir(name)
-    last_pull_date_file = os.path.join(repo_dir, '.hg', 'last_pull_date');
+    last_pull_date_file = os.path.join(repo_dir, '.hg', 'last_pull_date')
     if not os.path.exists(last_pull_date_file):
         return 0.0
 
@@ -87,7 +90,7 @@ def get_last_pull_date(name):
 def set_last_pull_date(name):
     timestamp = time.time()
     repo_dir = get_repo_dir(name)
-    last_pull_date_file = os.path.join(repo_dir, '.hg', 'last_pull_date');
+    last_pull_date_file = os.path.join(repo_dir, '.hg', 'last_pull_date')
     with open(last_pull_date_file, 'w') as f:
         f.write(str(timestamp))
 
@@ -99,10 +102,20 @@ def get_commit_info(repository, commit_id):
         if 'unknown revision' in e.stderr:
             return False
 
-def get_commits_info(repository, revsets):
-    rev = "(" + ") or (".join(revsets) + ")"
-    log = repository.log(rev=rev)
-    return log
+def get_revsets_info(repository, revsets):
+    return map(lambda revset: get_revset_info(repository, revset), revsets)
+
+def get_revset_info(repository, revset):
+    commits = repository.log(rev=revset)
+    commits_count = len(commits)
+    if commits_count > MAX_COMMITS:
+        msg = too_many_commits_error(commits_count, MAX_COMMITS)
+        raise TooManyCommitsError(msg)
+
+    return {
+        "revset": revset,
+        "commits": commits
+    }
 
 def cleanup(repo):
     logger.info('cleaning up')
@@ -115,7 +128,7 @@ def cleanup(repo):
         if 'empty revision set' not in e.stderr:
             raise e
 
-def raw_transplant(repository, source, commit_id, message=None):
+def raw_transplant(repository, source, revset, message=None):
     filter = None
     env = os.environ.copy()
 
@@ -123,16 +136,16 @@ def raw_transplant(repository, source, commit_id, message=None):
         filter = TRANSPLANT_FILTER
         env['TRANSPLANT_MESSAGE'] = message
 
-    return repository.transplant(commit_id, source=source, filter=filter, env=env)
+    return repository.transplant(revset, source=source, filter=filter, env=env)
 
-def do_transplant(src, dst, commits):
+def transplant(src, dst, items):
     try:
         clone_or_pull(src, refresh=True)
         dst_repo = clone_or_pull(dst, refresh=True)
 
         try:
-            for commit in commits:
-                do_transplant_commit(src, dst, commit)
+            for item in items:
+                transplant_item(src, dst, item)
 
             logger.info('pushing "%s"', dst)
             dst_repo.push()
@@ -142,7 +155,8 @@ def do_transplant(src, dst, commits):
             return jsonify({'tip': tip})
 
         finally:
-            cleanup(dst_repo)
+            #cleanup(dst_repo)
+            pass
 
     except MercurialException, e:
         print e
@@ -156,32 +170,74 @@ def do_transplant(src, dst, commits):
             }
         }), 409
 
+def transplant_item(src, dst, item):
+    if 'commit' in item:
+        transplant_commit(src, dst, item)
+    elif 'revset' in item:
+        transplant_revset(src, dst, item)
+    else:
+        raise Exception("unknown item: {}".format(item))
 
-def do_transplant_commit(src, dst, commit):
+def transplant_commit(src, dst, item):
+    message = item.get('message', None)
+    _transplant(src, dst, item['commit'], message=message)
+
+def transplant_revset(src, dst, item):
+    message = item.get('message', None)
+
+    src_repo = Repository(get_repo_dir(src))
+    dst_repo = Repository(get_repo_dir(dst))
+    commits = src_repo.log(rev=item['revset'])
+    commits_count = len(commits)
+    if commits_count > MAX_COMMITS:
+        msg = too_many_commits_error(commits_count, MAX_COMMITS)
+        raise TooManyCommitsError(msg)
+
+    if commits_count == 0:
+        return
+
+    if commits_count == 1:
+      _transplant(src, dst, item['revset'], message=message)
+    else:
+      old_tip = dst_repo.id(id=True)
+      revset = [commit['node'] for commit in commits]
+
+      # no need to pass message as we'll override it during collapse anyway
+      _transplant(src, dst, revset)
+
+      collapse_rev = 'descendants(children({}))'.format(old_tip)
+      collapse_commits = dst_repo.log(rev=collapse_rev)
+
+      # less than two commits were transplanted, no need to squash
+      if len(collapse_commits) < 2:
+        return
+
+      logger.info('collapsing "%s"', collapse_rev)
+      dst_repo.collapse(rev=collapse_rev, message=message)
+
+
+def _transplant(src, dst, revset, message=None):
     dst_repo = Repository(get_repo_dir(dst))
     src_dir = get_repo_dir(src)
 
-    logger.info('transplanting revision "%s" from "%s" to "%s"', commit['id'], src, dst)
+    logger.info('transplanting "%s" from "%s" to "%s"', revset, src, dst)
 
-    if 'message' not in commit:
-        result = raw_transplant(dst_repo, src_dir, commit['id'])
-    else:
-        result = raw_transplant(dst_repo, src_dir, commit['id'], message=commit['message'])
+    result = raw_transplant(dst_repo, src_dir, revset, message=message)
+    dst_repo.update()
 
     logger.debug('hg transplant: %s', result)
 
-def too_many_commits_error(commits_count):
-    return jsonify({
-        'error': "You're trying to transplant {} commits which is above {} commits limit".format(commits_count, MAX_COMMITS)
-    }), 400
+
+def too_many_commits_error(current, limit):
+    return "You're trying to transplant {} commits which is above {} commits limit".format(current, limit)
 
 @app.route('/')
-def index():
+def flask_index():
     repositories = app.config['REPOSITORIES']
     return render_template('index.html', repositories=repositories)
 
-@app.route('/repositories/<repository_id>/commits/')
-def show_commits(repository_id):
+@app.route('/repositories/<repository_id>/revsets')
+def flask_get_revsets(repository_id):
     revsets = request.values.get('revsets')
     if not revsets:
         return jsonify({'error': 'No revsets'}), 400
@@ -189,29 +245,29 @@ def show_commits(repository_id):
 
     repository = clone_or_pull(repository_id)
     try:
-        commits_info = get_commits_info(repository, revsets)
+        revsets_info = get_revsets_info(repository, revsets)
+    except TooManyCommitsError, e:
+        return jsonify({
+            'error': e.message
+        }), 400
     except MercurialException, e:
         return jsonify({
             'error': e.stderr
         }), 400
 
-    commits_count = len(commits_info)
-    if commits_count > MAX_COMMITS:
-        return too_many_commits_error(commits_count);
-
     return jsonify({
-        'commits': commits_info
+        'revsets': revsets_info
     })
 
 @app.route('/transplant', methods = ['POST'])
-def transplant():
+def flask_transplant():
     params = request.get_json()
     if not params:
         return jsonify({'error': 'No src'}), 400
 
     src = params.get('src')
     dst = params.get('dst')
-    commits = params.get('commits')
+    items = params.get('items')
 
     if not src:
         return jsonify({'error': 'No src'}), 400
@@ -219,8 +275,8 @@ def transplant():
     if not dst:
         return jsonify({'error': 'No dst'}), 400
 
-    if not commits:
-        return jsonify({'error': 'No commits'}), 400
+    if not items:
+        return jsonify({'error': 'No items'}), 400
 
     if not has_repo(src):
         msg = 'Unknown src repository: {}'.format(src)
@@ -234,11 +290,10 @@ def transplant():
         msg = 'Transplant from {} to {} is not allowed'.format(src, dst)
         return jsonify({'error': msg}), 400
 
-    commits_count = len(commits)
-    if commits_count > MAX_COMMITS:
-        return too_many_commits_error(commits_count);
+    return transplant(src, dst, items)
 
-    return do_transplant(src, dst, commits)
+class TooManyCommitsError(Exception):
+    pass
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
